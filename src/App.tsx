b156e97c928f62ad2a36acc02fb143e3cb5b6152
leonardo fleitas <leoflalv.dev@gmail.com>
2026-06-16 23:25:17 +0200

    add initial configuration

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="min-h-screen bg-transparent p-4">
+      <h1 className="text-xl font-bold text-white">Goals Tracker</h1>
+    </main>
+  );
+}
+
+export default App;
